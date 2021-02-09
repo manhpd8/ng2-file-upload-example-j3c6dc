9fc76157f97eda78560a4fb967c0b94e9d3bc53e
manhpd8 <phamducmanh.work@gmail.com>
2021-02-09 17:09:50 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ng2-file-upload-example-j3c6dc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ng2-file-upload-example-j3c6dc)
---
+++ app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter } from '@angular/core';
+import { FileUploader, FileLikeObject } from 'ng2-file-upload';
+ 
+
+const URL = 'http://localhost:3000/fileupload/';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  
+ public uploader: FileUploader = new FileUploader({
+    url: URL
+
+    });
+
+  public onFileSelected(event: EventEmitter<File[]>) {
+    const file: File = event[0];
+    console.log(file);
+
+  }
+}
